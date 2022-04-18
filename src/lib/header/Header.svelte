8b746d959f72364c02f8d5ceeb62498d3344d0ee
xbucks <jmy.kot@gmail.com>
2022-04-18 12:55:43 +0900

    add initial examples page

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -19,6 +19,9 @@
 			<li class:active={$page.url.pathname === '/about'}>
 				<a sveltekit:prefetch href="/about">About</a>
 			</li>
+			<li class:active={$page.url.pathname === '/examples'}>
+				<a sveltekit:prefetch href="/examples">Examples</a>
+			</li>
 			<li class:active={$page.url.pathname === '/todos'}>
 				<a sveltekit:prefetch href="/todos">Todos</a>
 			</li>
