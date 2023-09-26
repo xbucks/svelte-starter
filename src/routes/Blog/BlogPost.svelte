abd011fca0234fa44c9a2a55cff2f93caf65151c
xbucks <jmy.kot@gmail.com>
2023-09-26 11:37:00 +0900

    fix: clean up code base

--- src/routes/Blog/BlogPost.svelte
+++ src/routes/Blog/BlogPost.svelte
@@ -1,19 +1,17 @@
 <script>
-	export let id
+  export let id;
 
-    import { Button } from 'sveltestrap'
-    import { navigate } from "svelte-routing"
-    import Counter from 'src/lib/Counter.svelte'
+  import { Button } from "sveltestrap";
+  import { navigate } from "svelte-routing";
+  import Counter from "src/lib/Counter.svelte";
 
-    const gotohome = () => {
-        navigate("/", { replace: true })
-    }
+  const gotohome = () => {
+    navigate("/", { replace: true });
+  };
 </script>
 
 <h1>BlogPost {id}</h1>
 
 <Counter />
 
-<Button color="secondary" on:click={gotohome}>
-    Go to Home
-</Button>
+<Button color="secondary" on:click={gotohome}>Go to Home</Button>
