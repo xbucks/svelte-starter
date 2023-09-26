039d5a0e05da8c391703741ac237240a27d0634c
xbucks <jmy.kot@gmail.com>
2023-09-26 11:22:33 +0900

    fix: clean up code base

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
-    resolve: {
-        alias: {
-            src: '/src',
-        },
-    },
+	plugins: [svelte()],
+	resolve: {
+		alias: {
+			src: '/src'
+		}
+	}
 })
