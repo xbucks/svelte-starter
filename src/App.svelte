7d9f3fbf96184c8809369767c9ff06a7baa8d23e
xbucks <jmy.kot@gmail.com>
2023-09-26 18:36:36 +0900

    feat: add public && private routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,44 @@
 <!-- App.svelte -->
 <script>
   import { Router, Link, Route } from "svelte-routing";
+  import { token } from "src/stores/app";
   import Home from "src/routes/Home.svelte";
   import About from "src/routes/About.svelte";
   import Blog from "src/routes/Blog/Blog.svelte";
   import BlogPost from "src/routes/Blog/BlogPost.svelte";
+  import Login from "src/routes/Login.svelte";
+  import Register from "src/routes/Register.svelte";
 
   export let url = "";
 </script>
 
-<Router {url}>
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/blog">Blog</Link>
-  </nav>
-  <div>
-    <Route path="/blog/:id" let:params>
-      <BlogPost id={params.id} />
-    </Route>
-    <Route path="/blog" component={Blog} />
-    <Route path="/about" component={About} />
-    <Route path="/"><Home /></Route>
-  </div>
-</Router>
+{#if $token}
+  <Router {url}>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/blog">Blog</Link>
+    </nav>
+    <div>
+      <Route path="/blog/:id" let:params>
+        <BlogPost id={params.id} />
+      </Route>
+      <Route path="/blog" component={Blog} />
+      <Route path="/about" component={About} />
+      <Route path="/"><Home /></Route>
+    </div>
+  </Router>
+{:else}
+  <Router {url}>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+    </nav>
+    <div>
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/"><Home /></Route>
+    </div>
+  </Router>
+{/if}
---
+++ src/routes/Login.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Form, FormGroup, FormText, Input, Label } from "sveltestrap";
+</script>
+
+<Form>
+  <FormGroup>
+    <Label for="exampleEmail">Email</Label>
+    <Input
+      type="email"
+      name="email"
+      id="email"
+      placeholder="with a placeholder"
+    />
+  </FormGroup>
+  <FormGroup>
+    <Label for="examplePassword">Password</Label>
+    <Input
+      type="password"
+      name="password"
+      id="password"
+      placeholder="password placeholder"
+    />
+  </FormGroup>
+</Form>
