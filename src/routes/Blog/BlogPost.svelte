706dde8210e689f4d7199b76292ff8b92190498c
xbucks <jmy.kot@gmail.com>
2023-09-26 11:06:30 +0900

    feat: add navigate func

--- src/routes/Blog/BlogPost.svelte
+++ src/routes/Blog/BlogPost.svelte
@@ -1,5 +1,19 @@
 <script>
-	export let id;
+	export let id
+
+    import { Button } from 'sveltestrap'
+    import { navigate } from "svelte-routing"
+    import Counter from '../../lib/Counter.svelte'
+
+    const gotohome = () => {
+        navigate("/", { replace: true })
+    }
 </script>
 
 <h1>BlogPost {id}</h1>
+
+<Counter />
+
+<Button color="secondary" on:click={gotohome}>
+    Go to Home
+</Button>
